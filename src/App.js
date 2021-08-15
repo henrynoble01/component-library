8e937750037eb8d8728d7ac2dd5999449e531b9d
henrynoble01 <henrynoble01@gmail.com>
2021-08-15 07:38:57 +0100

    chore: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import EcoPrefrences from "./pages/EcoPrefrences";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'> Home </Link>
+            </li>
+            <li>
+              <Link to='/EcoPrefrences'> EcoPrefrences </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/EcoPrefrences' component={EcoPrefrences} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
